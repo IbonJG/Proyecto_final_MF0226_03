b4690851e2a5fb033dcdfd6b5eb524c81b0ab456
IbonJG <ibonjg86@gmail.com>
2022-04-06 12:53:41 +0100

    Insertar Alumno, sin terminar

---
+++ src/main/java/com/elorrieta/proyectofinal/InsertarAlumno.java
@@ -0,0 +1,77 @@
+package com.elorrieta.proyectofinal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class InsertarAlumno {
+
+	public static void main(String[] args) throws SQLException {
+
+		// no hay resulSet porque la consulta no devuelve ningun resultado, no es una
+		// SELECT
+		// lo que tiene que haber es executeUpdate porque es una consulta de INSERT
+
+		// creracion de las variables que necesitamos para recoger los datos
+		String nombre;
+		String apellidos;
+		// preparar el scaner para recoger los datos introducidos por teclado
+		Scanner sc = new Scanner(System.in);
+
+		try (Connection con = Conexion.getConnection();
+				PreparedStatement pst = con.prepareStatement("INSERT INTO alumno (nombre, apellidos) VALUES (?, ?)");) {
+
+			// nombre vacio
+			boolean nombreVacio = true;
+			do {
+				// pedir datos por teclado y guardar en variables
+				System.out.println("Introduce un nombre");
+				nombre = sc.nextLine().trim();
+
+				if ("".equalsIgnoreCase(nombre) || nombre.length() > 45) {
+
+					System.out.println("El nombre no puede estar vacio o superar 45 caracteres");
+				}
+
+				else {
+					// asignar dato introducido de variable a interrogante de sql
+					pst.setString(1, nombre);
+					nombreVacio = false;
+				}
+
+			} while (nombreVacio);
+
+			// apellido vacio
+			boolean apellidoVacio = true;
+			do {
+				// pedir datos por teclado y guardar en variables
+				System.out.println("Introduce los apellidos");
+				apellidos = sc.nextLine().trim();
+
+				if ("".equalsIgnoreCase(apellidos) || apellidos.length() > 150) {
+
+					System.out.println("El nombre no puede estar vacio o superar 150 caracteres");
+				}
+
+				else {
+					// asignar dato introducido de variable a interrogante de sql
+					pst.setString(2, apellidos);
+					apellidoVacio = false;
+				}
+
+			} while (apellidoVacio);
+
+			pst.executeUpdate();
+			System.out.println("Alumno insertado\n");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		// cerrar el teclado
+		sc.close();
+
+	}
+
+}
